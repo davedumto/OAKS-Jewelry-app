fcd8baa6cbaf6612dcd2f3c71acd51498d10952b
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2024-03-27 20:11:23 +0100

    I created a src/pages and src/components, and i also installed the tailwind config

---
+++ Oakz-jewelry-app/src/data/productList.js
@@ -0,0 +1,15 @@
+// src/data/products.js
+
+// Define an array containing product categories
+const products = [
+    "necklaces",
+    "rings",
+    "wrist watches",
+    "waist beads",
+    "earrings",
+    "bracelets"
+  ];
+  
+  // Export the array
+  export default products;
+  
