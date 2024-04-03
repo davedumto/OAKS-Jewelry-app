ab2e11f1cf2b177eb73d7db11bde243d7a3a7e9d
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2024-04-03 23:30:55 +0100

    I made some adjsutments based on the review gotten from my colleagues

--- Oakz-jewelry-app/src/components/productSection.jsx
+++ Oakz-jewelry-app/src/components/productSection.jsx
@@ -5,7 +5,7 @@ import productItems from "../data/productItem";
 
 const ProductSection = () => {
   return (
-    <div className="flex flex-col w-full px-[30px]">
+    <div className="flex flex-col w-full px-[64px] py-[64px] ">
       <div className="flex flex-col items-start">
         <h1 className="text-[48px] ">
           <span className="text-orange-400 ">Dorn</span> yourself with our best
@@ -18,10 +18,10 @@ const ProductSection = () => {
 
       <div className="flex justify-center py-[32px]">
         {" "}
-        <ul className="text-red-400 flex  justify-around border-[3px] border-[#D76F30] p-[10px] px-[20px] rounded-[32px] bg-[#EFEFD0] w-[690px]">
+        <ul className="text-red-400 flex  justify-around border-[3px] border-[#D76F30] p-[10px]  rounded-[32px] bg-[#EFEFD0] w-[690px]">
           {productList.map((product, index) => (
             <li
-              className="text-[16px] text-black active:bg-[#D76F30] active:text-white active:rounded-[32px] p-2 focus:bg-[#D76F30] focus:text-white focus:rounded-[32px] hover:bg-[#D76F30] hover:text-white hover:rounded-[32px]"
+              className="text-[16px] cursor-pointer text-black active:bg-[#D76F30] active:text-white active:rounded-[32px] p-2  focus:bg-[#D76F30] focus:text-white focus:rounded-[32px] hover:bg-[#D76F30] hover:text-white hover:rounded-[32px]"
               key={index}
             >
               {product}
@@ -29,13 +29,18 @@ const ProductSection = () => {
           ))}
         </ul>
       </div>
-      <div className="grid grid-cols-5 gap-6 px-[64px] ">
+      <div className="grid grid-cols-5 gap-6  ">
         {productItems.map((product, index) => (
           <div
             key={index}
-            className="flex flex-col items-center bg-[#EFEFD0] h-[270px] w-full "
+            className="flex flex-col items-center bg-[#EFEFD0] h-[250px] w-full rounded-xl "
           >
-            <img src={product.imgSrc} alt={product.name} className="w-full" />
+            <img
+              src={product.imgSrc}
+              alt={product.name}
+              className="object-contain w-full h-auto rounded-t-xl"
+            />
+
             <div className="flex flex-col w-full justify-around h-[50%] px-2">
               <div className="flex justify-between w-full">
                 <p className="text-[14px]">{product.name}</p>
@@ -53,9 +58,17 @@ const ProductSection = () => {
               <div className="flex justify-between w-full">
                 <p>NGN: {product.price}</p>
 
-                <a href="">
-                  <img src="/images/add.svg" className="h-[24px]" alt="" />
-                </a>
+                {product.stock === "IN STOCK" ? (
+                  <a href="">
+                    <img src="/images/add.svg" className="h-[32px]" alt="" />
+                  </a>
+                ) : (
+                  <img
+                    src="/images/add.svg"
+                    className="h-[32px] opacity-50 cursor-not-allowed"
+                    alt=""
+                  />
+                )}
               </div>
             </div>
           </div>
