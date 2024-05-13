39afa403b1666d929e76bbee627b85f4856efbcd
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2024-05-13 11:31:03 +0100

    I made some changes

--- Oakz-jewelry-app/src/pages/checkout.jsx
+++ Oakz-jewelry-app/src/pages/checkout.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import OAKZ_WHITE from "/images/OAKZ_white.svg";
 import ArrowRoundedBack from "/images/mingcute.svg";
-import { LocalGroceryStoreOutlined, Search } from "@mui/icons-material";
+import {
+  AddCircleOutline,
+  LocalGroceryStoreOutlined,
+  Search,
+} from "@mui/icons-material";
 import { Link } from "react-router-dom";
 import Footer from "../components/footer";
+import fields from "../data/fields";
 
 const Checkout = () => {
+  const [file, setFile] = useState(null);
+  const handleFileChange = (e) => {
+    const selectedFile = e.target.files[0];
+    if (selectedFile) {
+      if (
+        selectedFile.type === "application/pdf" ||
+        selectedFile.type.startsWith("image/")
+      ) {
+        setFile(selectedFile);
+      } else {
+        alert("Please upload either an image file or a PDF.");
+      }
+    }
+  };
   return (
     <div>
       <div className="flex flex-col gap-[2em] px-[2em] py-[1.5em] md:px-[3em] h-screen">
@@ -78,12 +97,34 @@ const Checkout = () => {
             </div>
           </div>
           <div className="flex shadow-2xl flex-1 gap-[0.5em] justify-between  ">
-            <div className="flex flex-col justify-between">
-              <p>Input your details for identification</p>
-              <button className="w-full bg-[#172D13]">Confirm details</button>
+            <div className="flex flex-col justify-between flex-1 pt-[2em]  pb-[4em] ">
+              <div className="flex flex-col gap-[1em] border-r-[1px] border-black px-[1.5em] ">
+                {" "}
+                <p className="text-[2em] leading-9 ">
+                  Input your details for identification
+                </p>
+                <div className="flex flex-col w-full gap-[0.4em]">
+                  {fields.map((field, index) => (
+                    <div
+                      key={index}
+                      className="w-full border-[1px] rounded-lg border-black p-2"
+                    >
+                      <p>{field.label}</p>
+                      <input
+                        className="w-full"
+                        type="text"
+                        placeholder={field.placeholder}
+                      />
+                    </div>
+                  ))}
+                  <button className="w-full bg-[#172D13] mt-[0.5em] text-white p-2 rounded-lg cursor-pointer">
+                    Confirm details
+                  </button>
+                </div>
+              </div>
             </div>
-            <div className="w-[1px] h-80% bg-black"></div>
-            <div>
+
+            <div className="flex-1 px-[1.5em] pt-[2em]  pb-[4em] ">
               <p>Transfer to the account bellow </p>
               <div>
                 <ul>
@@ -99,6 +140,33 @@ const Checkout = () => {
                     <span>MoniePoint</span>
                   </li>
                 </ul>
+              </div>
+              <div className="text-center">
+                <div className="mb-4">
+                  <label htmlFor="file-upload" className="cursor-pointer block">
+                    <div className="flex border-2 border-dashed border-gray-300 p-4 rounded-lg w-64 mx-auto">
+                      <div className="border-[1px] border-black rounded-[50%] ">
+                        {file && (
+                          <p className="text-gray-700 w-[2em] overflow-hidden overflow-ellipsis whitespace-nowrap p-1">
+                            {file.name}
+                          </p>
+                        )}
+                      </div>{" "}
+                      <div className="text-gray-b ">
+                        <AddCircleOutline className="text-2xl"/>
+                      </div>
+                    </div>
+                  </label>
+                  <input
+                    id="file-upload"
+                    type="file"
+                    accept="image/*,.pdf"
+                    className="hidden"
+                    onChange={handleFileChange}
+                  />
+                </div>
+
+                <p className="text-sm text-gray-500">Upload proof of payment</p>
               </div>
             </div>
           </div>
