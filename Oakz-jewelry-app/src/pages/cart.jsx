6d581f29f828e3d62d24a4a42fb28109c738c482
Nwanne-san <113237530+Nwanne-san@users.noreply.github.com>
2024-06-14 02:04:26 +0100

    cart page routing and modal responsiveness implementation

--- Oakz-jewelry-app/src/pages/cart.jsx
+++ Oakz-jewelry-app/src/pages/cart.jsx
@@ -4,12 +4,14 @@ import Oakz from '../../public/images/OAKZ(1).svg';
 import LocalGroceryStoreTwoToneIcon from '@mui/icons-material/LocalGroceryStoreTwoTone';
 import Return from '../../public/images/return-icon.svg';
 import CartPage2 from '../components/cartpage2';
+import ArrowRoundedBack from "/images/mingcute.svg";
+import { Link } from 'react-router-dom';
 
 export default function Cart() {
 
   return (
     <>
-      <div className=' sm:py-[40px] py-[30px] sm:px-[70px] px-[20px]'>
+      <div className=' sm:py-[40px] py-[30px] sm:px-[70px] px-[16px]'>
         <div className='flex flex-col gap-[22px]'>
           <div className='flex justify-between items-center'>
             <img src={Oakz} alt="Oakz-logo" />
@@ -17,12 +19,17 @@ export default function Cart() {
               <LocalGroceryStoreTwoToneIcon fontSize="medium" />
             </span>
           </div>
-          <div className='bg-beige py-3 sm:px-8 px-6 flex justify-between items-center'>
+          <div className='bg-beige py-3 sm:px-8 px-4 flex justify-between items-center'>
             <p className='font-bienvenido sm:text-3xl text-2xl leading-[37.44px]'>Cart</p>
-            <div className='flex gap-2 items-center'>
-              <span><img src={Return}/></span>
-              <span className='sm:text-3xl text-2xl leading-[37.44px] text-green font-bienvenido'>Go back</span>
-            </div>
+            <Link to={"/"}>  
+              <div className='flex gap-1 sm:gap-2 items-center'>
+              
+                {""}
+                <span><img src={ArrowRoundedBack} className=''/></span>
+                <span className='sm:text-3xl text-xl leading-[37.44px] text-green font-bienvenido'>Go back</span>
+              
+              </div>
+            </Link>
           </div>
         </div>
         <CartPage2/>
