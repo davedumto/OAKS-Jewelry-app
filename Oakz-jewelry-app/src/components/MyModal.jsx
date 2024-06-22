c4662f780f6385ff10e5fcfbe0c4c08542619fee
Chibuikem Michael Ilonze <chibuikemichaelilonze@gmail.com>
2024-06-22 16:01:12 +0100

    I added colors

--- Oakz-jewelry-app/src/components/MyModal.jsx
+++ Oakz-jewelry-app/src/components/MyModal.jsx
@@ -52,6 +52,12 @@ const MyModal = ({ isOpen, onRequestClose, product }) => {
 
   if (!product) return null;
 
+  const colorClasses = {
+    Gold: "bg-orange-300",
+    Silver: "bg-zinc-400",
+    Black: "bg-black",
+  };
+
   const customStyles = {
     overlay: {
       backgroundColor: "rgba(0, 0, 0, 0.5)",
@@ -94,7 +100,7 @@ const MyModal = ({ isOpen, onRequestClose, product }) => {
       style={customStyles}
     >
       <style>{responsiveStyles}</style>
-      <div className="flex flex-col md:flex-row gap-4 md:gap-2">
+      <div className="flex flex-col md:flex-row gap-4 md:gap-2 p-2">
         <div className="flex flex-col w-full md:w-1/3">
           <div className="flex justify-between items-start md:hidden mb-4">
             <h2 className="text-xl md:text-2xl">{product.name}</h2>
@@ -106,26 +112,12 @@ const MyModal = ({ isOpen, onRequestClose, product }) => {
             <img
               src={product.imgSrc}
               alt={product.name}
-              className="object-cover w-full h-48 md:w-[12rem] md:h-[14rem] mb-2 rounded-md"
+              className="object-cover w-full h-48 md:w-[12rem] md:h-[14rem] rounded-md"
             />
-            <div>
-              <ul className="flex justify-between">
-              <li className="bg-orange-300 text-[0.7rem] text-white rounded-md py-1 px-3 cursor-pointer">
-                  Gold
-                </li>
-                <li className="bg-zinc-400 text-[0.7rem] text-white rounded-md py-1 px-3 cursor-pointer">
-                  Silver
-                </li>
-                <li className="bg-black text-[0.7rem] text-white rounded-md py-1 px-3 cursor-pointer">
-                  Black
-                </li>
-              
-              </ul>
-            </div>
           </div>
           <p>{product.description}</p>
         </div>
-        <div className="flex flex-col w-full md:w-2/3 gap-4">
+        <div className="flex flex-col w-full md:w-2/3 gap-4 justify-between">
           <div className="hidden md:flex justify-between">
             <h2 className="text-xl md:text-2xl mb-2">{product.name}</h2>
             <div className="flex justify-start items-start">
@@ -134,30 +126,56 @@ const MyModal = ({ isOpen, onRequestClose, product }) => {
               </button>
             </div>
           </div>
-          <div className="flex flex-col md:flex-row gap-2 justify-between">
-            <div>
+          <div className="flex flex-col  md:flex-row gap-6 flex-1 h-full justify-between ">
+            <div className="flex flex-col gap-2 flex-1 justify-between">
               <p
-                className={`text-sm mb-4 ${
+                className={`text-sm  ${
                   product.stock === "IN STOCK"
-                    ? "text-green-600"
+                    ? "bg-brownbg flex items-center text-white text-[0.625em] rounded-md p-1 w-fit"
                     : "text-red-600"
                 }`}
               >
                 {product.stock}
               </p>
-              <p className="text-lg mb-2">NGN: {product.price}</p>
+              <div className="flex flex-col h-2/3 justify-between">
+                <p className="text-lg ">NGN: {product.price}</p>
+                <div className="flex justify-between  ">
+                  {product.colors.map((color) => (
+                    <a>
+                      {" "}
+                      <button
+                        key={color}
+                        className={`${colorClasses[color]} text-[0.7rem] focus:border-[1.5px] focus:border-zinc-600  text-white rounded-md py-1 px-2 cursor-pointer`}
+                      >
+                        {color}
+                      </button>
+                    </a>
+                  ))}
+                </div>
+                <div className="flex  justify-between w-3 gap-1">
+                  <button className=" text-[0.7rem]  flex justify-center items-center border-[1px]  focus:border-[1.5px] focus:border-zinc-700 border-zinc-400 rounded-md py-1 px-3 cursor-pointer">
+                    S
+                  </button>
+                  <button className=" text-[0.7rem]  flex justify-center items-center border-[1px] focus:border-[1.5px] focus:border-zinc-700 border-zinc-400 rounded-md py-1 px-3 cursor-pointer">
+                    M
+                  </button>
+                  <button className=" text-[0.7rem] flex justify-center items-center border-[1px] focus:border-[1.5px] focus:border-zinc-700 border-zinc-400 rounded-md py-1 px-3 cursor-pointer">
+                    L
+                  </button>{" "}
+                  <button className=" text-[0.7rem] flex justify-center items-center border-[1px] focus:border-[1.5px] focus:border-zinc-700 border-zinc-400 rounded-md py-1 px-3 cursor-pointer">
+                    XL
+                  </button>
+                </div>
+              </div>
             </div>
-            <div className="flex flex-col gap-4">
+            <div className="flex flex-col justify-between flex-1">
               <p className="bg-[#BB2929] text-sm py-1 px-3 text-white">
                 PROMO 35% OFF
               </p>
               <div className="flex flex-col gap-2">
-                <button className="bg-[#172D13] text-sm py-1 px-2 w-full text-white rounded-xl border border-white">
-                  Buy now
-                </button>
                 {quantity === 0 ? (
                   <button
-                    className="bg-white text-sm py-1 px-2 text-black rounded-xl border border-green-600"
+                    className="bg-white text-sm py-1 px-2 text-black rounded-xl border border-zinc-400"
                     onClick={handleAddToCart}
                   >
                     Add to cart
@@ -179,6 +197,9 @@ const MyModal = ({ isOpen, onRequestClose, product }) => {
                     />
                   </div>
                 )}
+                <button className="bg-[#172D13] text-sm py-1 px-2 w-full text-white rounded-xl border border-white">
+                  Buy now
+                </button>
               </div>
             </div>
           </div>
